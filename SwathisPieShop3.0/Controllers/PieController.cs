83aa3013e3b37629fbf45073b731c060b89ef38b
Anuresh Puthukudy <anureshmp@gmail.com>
2020-02-22 17:01:59 +1100

    Added Viewmodels, Controllers and Client side bootstrap, jquery libraries

---
+++ SwathisPieShop3.0/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SwathisPieShop3._0.Models;
+using SwathisPieShop3._0.ViewModels;
+
+namespace SwathisPieShop3._0.Controllers
+{
+    public class PieController: Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryInterface _categoryRepository;
+
+        public PieController(IPieRepository PieRepository, ICategoryInterface CategoryRepository)
+        {
+            _pieRepository = PieRepository;
+            _categoryRepository = CategoryRepository;
+
+        }
+
+        public ViewResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+           
+            return View(piesListViewModel);
+        }
+
+
+    }
+}
---
+++ SwathisPieShop3.0/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SwathisPieShop3._0.Models;
+
+namespace SwathisPieShop3._0.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+
+    }
+}
