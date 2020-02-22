a15563278b48380e612fb308b156c1ce42545c86
Anuresh Puthukudy <anureshmp@gmail.com>
2020-02-22 15:31:00 +1100

    Added model classes and images, configured services to use mock repositories

---
+++ SwathisPieShop3.0/Models/ICategoryInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwathisPieShop3._0.Models
+{
+    public interface ICategoryInterface
+    {
+        IEnumerable<Category> AllCategories { get; } 
+    }
+}
