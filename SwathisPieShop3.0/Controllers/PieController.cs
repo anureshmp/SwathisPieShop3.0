44fb78805798e67e9742aaa19435de4a880ac32f
Anuresh Puthukudy <anureshmp@gmail.com>
2020-02-23 12:51:36 +1100

    Added Navigation to the site. Used tag helpers.

--- SwathisPieShop3.0/Controllers/PieController.cs
+++ SwathisPieShop3.0/Controllers/PieController.cs
@@ -29,6 +29,17 @@ namespace SwathisPieShop3._0.Controllers
             return View(piesListViewModel);
         }
 
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+
+            if (pie == null)
+                return NotFound();
+
+            return View(pie);
+
+        }
+
 
     }
 }
