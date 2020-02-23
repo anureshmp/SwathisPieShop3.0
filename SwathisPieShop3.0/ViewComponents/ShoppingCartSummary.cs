5fc20876dad541aa685ded7246a44e28484c48a3
Anuresh Puthukudy <anureshmp@gmail.com>
2020-02-23 20:48:47 +1100

    Added Shopping cart summary, used Viewcomponent to implement this feature. Added a custom tag helper for email functionality

---
+++ SwathisPieShop3.0/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SwathisPieShop3._0.Models;
+using SwathisPieShop3._0.ViewModels;
+
+namespace SwathisPieShop3._0.ViewComponents
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+                
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+
+        }
+
+
+
+    }
+}
