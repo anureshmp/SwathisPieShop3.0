f0c6004f21728c65e8b5cbfad58e29d01e6f8ec1
Anuresh Puthukudy <anureshmp@gmail.com>
2020-02-23 17:42:55 +1100

    Added homepage, improved navigation used bootstrap carousel in the home page

---
+++ SwathisPieShop3.0/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SwathisPieShop3._0.Models;
+using SwathisPieShop3._0.ViewModels;
+
+namespace SwathisPieShop3._0.Controllers
+{
+    public class HomeController:Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public ViewResult Index()
+        {
+
+            var homeviewmodel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOftheWeek
+            };
+
+            return View(homeviewmodel);
+        }
+
+
+
+    }
+}
---
+++ SwathisPieShop3.0/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SwathisPieShop3._0.Models;
+
+namespace SwathisPieShop3._0.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+
+    }
+}
